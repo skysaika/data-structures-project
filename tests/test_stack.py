abbdee16456881df036c31ba7e03ea0c7f40fafb
Saysuu <skysaika@yandex.ru>
2023-03-23 17:18:43 +0300

    исправила тесты push, pop

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,51 +1,36 @@
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
 import unittest
 
-from src import stack
-from src.stack import Node
+from src.stack import Stack, Node
 
 
 
 class TestStack(unittest.TestCase):
     """Тесты для модуля stack."""
+
+    def __init__(self, methodName: str):
+        super().__init__(methodName)
+        self.stack = None
+
     def test_push(self):
-        """Проверка добавления элемента в начало списка."""
-        # self.assertIsInstance('stack.top', Node)
-        self.assertEqual('stack.top.data', 'test_data')
-        self.assertIs('stack.top.next_node', None)
+        """Тестируем push."""
+        stack = Stack()
+        stack.push('testdata')
+        self.assertEqual(stack.top.data, 'testdata')
+        self.assertIsInstance(stack.top, Node)
+        self.assertEqual(stack.top.data, 'testdata')
+        self.assertIs(stack.top.next_node, None)
         stack.push('testdata1')
-        self.assertIsInstance('stack.top.next_node', Node)
-        self.assertEqual('stack.top.data', 'testdata1')
-        self.assertIs('stack.top.next_node', None)
+        self.assertIsInstance(stack.top.next_node, Node)
+        self.assertEqual(stack.top.data, 'testdata1')
+
+    def test_pop(self):
+        """Тестируем pop."""
+        stack = Stack()
+        stack.push('testdata_1')
+        node_2 = stack.top
+        self.assertIs(stack.pop(), node_2.data)
+        with self.assertRaises(AttributeError):
+            stack.pop()
 
 
-if __name__ == '__main__':
-    unittest.main()
-
-
-# import pytest
-#
-# from src.stack import Stack, Node
-#
-#
-# def test_push(stack, node):
-#     assert isinstance(stack.top, Node)
-#     assert stack.top.data == 'test_data'
-#     assert stack.top.next_node is None
-#     stack.push('testdata1')
-#     assert isinstance(stack.top.next_node, Node)
-#     assert stack.top.data == 'testdata1'
-#     assert stack.top.next_node is node
-#
-#
-# def test_pop(stack, node):
-#     """Проверка удаления элемента из стека."""
-#     stack.push('testdata_1')
-#     node_2 = stack.top
-#     assert stack.pop() is node_2.data
-#     assert stack.pop() is node.data
-#     with pytest.raises(AttributeError):
-#         stack.pop()
-#
-#
-# Node('testdata', None)
