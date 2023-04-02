5475fd833b1c9a6d56d0920f3e34808de4d03bc3
Saysuu <skysaika@yandex.ru>
2023-04-02 12:42:44 +0300

    добавлен метод def dequeue в src/queue.py и тесты

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -13,6 +13,13 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(test_queue.head.data, 'data_1')
         self.assertEqual(test_queue.head.next_node.data, 'data_2')
 
+    def test_dequeue(self):
+        test_queue = queue.Queue()
+        test_queue.enqueue('data_1')
+        test_queue.enqueue('data_2')
+        self.assertEqual(test_queue.dequeue(), 'data_1')
+        self.assertEqual(test_queue.dequeue(), 'data_2')
+
     def test_str_queue(self):
         test_queue = queue.Queue()
         test_queue.enqueue('data_1')
